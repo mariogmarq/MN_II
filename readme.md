453ef1086d80f4b6726cf15c0a66ae3b4cf7d8ed
mariogmarq <mariogarciamarq@gmail.com>
2022-02-24 10:21:41 +0100

    Biseccion

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Metodos Numericos II
+
+En este repositorio dentro de la carpeta src podras encontrar implementaciones en el lenguaje de programacion Julia de los algoritmos estudiados en la asignatura de metodos numericos II de la universidad de Granada.
---
+++ src/biseccion.jl
@@ -0,0 +1,42 @@
+
+# Metodo de la biseccion para encontrar un cero de una funcion f
+# Devuelve un intervalo de longitud maxima ε que contiene un cero de la funcion
+# 6 digitos de precision por defecto
+# Requisitos para poder usar la funcion
+# 1) a < b
+# 2) f(a)f(b) < 0
+function biseccion(a::Number, b::Number, f::Function, ε::AbstractFloat = 10e-6, maxiter::Integer = 100000)
+    fa = f(a)
+    i = 0
+
+    if fa*f(b) > 0
+        throw(DomainError((a, b), "f(a) and f(b) have the same sign"))
+    end
+
+    while (b - a) > ε
+        i += 1
+        if i > maxiter
+            break
+        end
+
+        xn = (b + a) / 2
+        yn = f(xn)
+
+        if abs(yn) < ε
+            return (xn, xn)
+
+        elseif yn*fa > 0
+            a = xn
+            fa = yn
+        else
+            b = xn
+        end
+    end
+
+    (a, b)
+end
+
+a = 0
+b = 5.0
+f(x) = x^2 - 5
+print(biseccion(a, b, f))
